b25366f6e012ea453ff4269799db3b1e386f3d2a
Michael monin <mt.monin@gmail.com>
2016-08-31 23:47:49 +0200

    added messages stats

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -3,10 +3,17 @@ class Admin::StatsController < AdminController
 
   def index
     get_stats_signup
+    get_stats_messages
   end
 
   def get_stats_signup
-    @user_count = User.all.count
-    @user = User.group_by_week(:created_at).count
+    @users_count = User.all.count
+    @users = User.group_by_week(:created_at).count
+  end
+
+  def get_stats_messages
+    messages = Message.joins(:user).where('users.email != ?', "hello@alacaza.fr")
+    @messages_count = messages.all.count
+    @messages = messages.group_by_week('messages.created_at').count
   end
 end
