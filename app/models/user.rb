12e2cd6a7767157e58b99938375b7e5fc2ed10be
Michael monin <mt.monin@gmail.com>
2016-09-05 23:36:03 +0200

    ride of floor door edit profil

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, :last_name, :floor, :door, presence: true
+  validates :first_name, :last_name, presence: true
+  validates_presence_of :floor, :door, :on => :create
   validates :token, uniqueness: true, allow_blank: true
   validate :verify_building_password
 
