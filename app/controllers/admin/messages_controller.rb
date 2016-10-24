ea9244ec113ab9f1ca4b57f724d47d80214d5b76
Michael monin <mt.monin@gmail.com>
2016-10-24 08:33:11 +0200

    can send message to district

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -34,11 +34,15 @@ class Admin::MessagesController < AdminController
   end
 
   def notify_buildings
-    params[:recipients_buildings].reject(&:empty?).each do |building|
+    if params["district"].present?
+      params[:recipients_buildings] = Building.where(district: params["district"]).pluck(:id)
+    end
+    params[:recipients_buildings].each do |building|
+      next building if building.blank?
       channel = Channel.where(building: building, channel_type: "main_group").last
       user = User.find_by_email("hello@alacaza.fr")
       @message = Message.create(user: user, building_id: building, channel_id: channel.id, body: params[:body])
-      message_notifier(@message)
+      # message_notifier(@message)
     end
     flash[:success] = "Messages bien envoyés"
     redirect_to :back
